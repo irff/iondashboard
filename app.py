fad561ad04f674733a619eeacf21bdab7d1fdbad
kandito <kandito.agung@ui.ac.id>
2015-05-28 19:03:45 +0700

    auth

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import config
 
 """import all controller from application.controllers"""
-from controllers.page import page
+from controllers.auth import auth
 from controllers.data import data
 """import more controller"""
 
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
 
 """register each controller as blueprint"""
-app.register_blueprint(page)
 app.register_blueprint(data)
+app.register_blueprint(auth)
 """register more controller"""
 
 if __name__ == '__main__':
---
+++ controllers/auth.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, render_template, abort
+from flask import Flask, session, redirect, url_for, escape, request, flash, get_flashed_messages
+from jinja2 import TemplateNotFound
+import hashlib
+import settings
+import requests
+from models.user import User
+
+auth = Blueprint('auth', __name__, template_folder='application/templates')
+
+@auth.route('/auth/login', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(username=username).first()
+
+        if user == None:
+            flash('Invalid username or password')
+            return redirect(url_for('auth.index'))
+        else:
+            hash_pass = hashlib.sha224(password).hexdigest()
+            if hash_pass == user.password:
+                r = requests.get(settings.API_ENDPOINT + "/token", auth=(username, password))
+                data = r.json()
+                session['username'] = request.form['username']
+                session['token'] = data["token"]
+                print session['token']
+                return redirect(url_for('data.show'))
+            else:
+                flash('Invalid username or password')
+                return redirect(url_for('auth.index'))
+    else:
+        try:
+            return render_template('auth/login.html')
+        except TemplateNotFound:
+            abort(404)
+
+@auth.route('/auth/logout', methods=["GET"])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('auth.index'))
+
+@auth.route('/auth/about', methods=["GET"])
+def about():
+    if 'username' not in session:
+        return redirect(url_for('auth.index'))
+    return render_template('auth/about.html')
