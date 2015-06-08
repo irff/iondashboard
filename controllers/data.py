de79b525e4e1f303a735f8d4f41e8921c2d97ce3
kandito <kandito.agung@ui.ac.id>
2015-06-08 17:00:25 +0700

    logout jika token habis

--- controllers/data.py
+++ controllers/data.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, abort
 from flask import Flask, session, redirect, url_for, escape, request, flash, get_flashed_messages
 from jinja2 import TemplateNotFound
+import settings
+import requests
 
 data = Blueprint('data', __name__, template_folder='application/templates')
 
@@ -14,6 +16,11 @@ def index():
 def show(data):
     if 'username' not in session:
         return redirect(url_for('auth.index'))
+
+    r = requests.get(settings.API_ENDPOINT + '/listmedia', auth=(session['token'], 'unused'))
+
+    if r.status_code != 200:
+        return redirect(url_for('auth.logout'))
 
     try:
         return render_template('data/%s.html' % data, var=data)
