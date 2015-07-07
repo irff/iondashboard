7f8e312c5ddb0cc0a53cd07b9182a91a612be964
Alfan Nur Fauzan <alpancs@gmail.com>
2015-07-07 15:20:28 +0700

    fix routing

--- controllers/social.py
+++ controllers/social.py
@@ -5,19 +5,56 @@ import settings
 import requests
 import io
 
-social = Blueprint('social', __name__, template_folder='application/templates')
 
-@social.route('/social/', defaults={'data': 'index'})
-def show(data):
-    if 'username' not in session:
-        return redirect(url_for('auth.index'))
+from config import *
+import database.dbkeyword as dbk
+import database.dbresult as dbr
 
-    r = requests.get(settings.API_ENDPOINT + '/listmedia', auth=(session['token'], 'unused'))
+import json, urllib
+import analytics.tweet as tweeta
 
-    if r.status_code != 200:
-        return redirect(url_for('auth.logout'))
 
-    try:
-        return render_template('dashboard/index.html')
-    except TemplateNotFound:
-        abort(404)
+
+social = Blueprint('social', __name__, template_folder='application/templates/streamer')
+
+@app.route("/social")
+def home():
+    return render_template('streamings.html')
+
+@app.route("/analyze/<keyword>")
+def analyze(keyword):
+  return render_template('analyze.html', keyword = keyword, encoded_keyword = urllib.quote(keyword, safe='~()*!.\''))
+
+
+
+# API
+@app.route("/api/stream", methods=['POST'])
+def apistream():
+  keyword = request.json['keyword']
+  status = request.json['status']
+  if any([x.keyword == keyword and x.processing for x in dbk.get()]):
+    return "%s is processing" % keyword
+  return json.dumps( dbk.set(request.json) )
+
+@app.route("/api/summary", methods=['GET'])
+def summary():
+  keywords = [x for x in dbk.get() if x.status != 'removed']
+  keywords.sort(lambda x, y: cmp(x.status, y.status))
+  return json.dumps( map(tweeta.getinfo, keywords) )
+
+@app.route("/api/analyze/freq/<keyword>", methods=['GET'])
+def apianalyze(keyword):
+  return json.dumps( tweeta.get_tweet_freq(keyword) )
+
+@app.route("/api/analyze/topmention/<keyword>", methods=['GET'])
+def topmention(keyword):
+  return json.dumps( tweeta.get_top_mention(keyword) )
+
+@app.route("/api/analyze/topposting/<keyword>", methods=['GET'])
+def topposting(keyword):
+  return json.dumps( tweeta.get_top_posting(keyword) )
+
+@app.route("/api/analyze/topretweet/<keyword>", methods=['GET'])
+def topretweet(keyword):
+  return json.dumps( tweeta.get_top_retweet(keyword) )
+
