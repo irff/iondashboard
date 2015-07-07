ac88045734d359d367e500aad940f7f10bbd11af
Alfan Nur Fauzan <alpancs@gmail.com>
2015-07-07 11:36:32 +0700

    adding database

--- database/ionstreamer.sql
+++ database/ionstreamer.sql
@@ -5,8 +5,6 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP DATABASE IF EXISTS `ionstreamer`;
-CREATE DATABASE `ionstreamer` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `ionstreamer`;
 
 DROP TABLE IF EXISTS `keyword`;
